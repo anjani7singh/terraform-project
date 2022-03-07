dc7131be706157819c172b9c89e6b491dd952cd0
Anjani Singh <anjani@scanta.io>
2022-03-07 15:11:06 +0530

    ec2 instance provision

--- main.tf
+++ main.tf
@@ -1,20 +1,154 @@
 provider "aws" {
   region = "ap-south-1"
 }
-# resource:
-resource "aws_vpc" "development_vpc" {
-  cidr_block = "10.0.0.0/16"
+# variables:
+
+variable "vpc_cidr_block" {}
+variable "subnet_cidr_block" {}
+variable "environment" {}
+variable "my_ip" {}
+variable "instance_type" {}
+variable "az" {}
+# resources:
+
+resource "aws_vpc" "server_vpc" {
+  cidr_block = var.vpc_cidr_block
+  tags       = { Name : "${var.environment}-vpc" }
 }
 
-resource "aws_subnet" " development_subnet1" {
-  vpc_id            = aws_vpc.development_vpc.id
-  cidr_block        = "10.0.10.0/24"
-  availability_zone = "ap-south-1a"
+resource "aws_subnet" "server_subnet" {
+  cidr_block        = var.subnet_cidr_block
+  vpc_id            = aws_vpc.server_vpc.id
+  availability_zone = var.az
+  tags              = { Name : "${var.environment}-subnet" }
 }
 
-output "aws_vpc_id" {
-  value = aws_vpc.development_vpc.id
+resource "aws_internet_gateway" "server-igw" {
+  vpc_id = aws_vpc.server_vpc.id
+  tags   = { Name : "${var.environment}-igw" }
+
 }
-output "aws_subnet" {
-  value = aws_subnet.development_subnet1.id
+/*
+resource "aws_route_table" "server-route-table" {
+  vpc_id = aws_vpc.server_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.server-igw.id
+  }
+  tags = { Name : "${var.environment}-route-table" }
 }
+
+# associate the subnet with route table:
+resource "aws_route_table_association" "association-rt-subnet" {
+  subnet_id      = aws_subnet.server_subnet.id
+  route_table_id = aws_route_table.server-route-table.id
+}
+*/
+resource "aws_default_route_table" "main-rt" {
+  default_route_table_id = aws_vpc.server_vpc.default_route_table_id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.server-igw.id
+  }
+  tags = { Name : "main-route-table" }
+}
+/*
+# to define a new security group:
+resource "aws_security_group" "server-sg" {
+  name   = "server-sg"
+  vpc_id = aws_vpc.server_vpc.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8080
+    protocol    = "tcp"
+    to_port     = 8080
+  }
+  ingress {
+    cidr_blocks = [var.my_ip]
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+  }
+  egress {
+    cidr_blocks     = ["0.0.0.0/0"]
+    protocol        = "-1"
+    from_port       = 0
+    to_port         = 0
+    prefix_list_ids = []
+  }
+  tags = { Name : "${var.environment}-sg" }
+
+}*/
+
+# to use the default security group of the vpc:
+resource "aws_default_security_group" "default-server-sg" {
+  vpc_id = aws_vpc.server_vpc.id
+  ingress {
+    cidr_blocks = ["0.0.0.0/0"]
+    from_port   = 8080
+    protocol    = "tcp"
+    to_port     = 8080
+  }
+  ingress {
+    cidr_blocks = [var.my_ip]
+    from_port   = 22
+    protocol    = "tcp"
+    to_port     = 22
+  }
+  egress {
+    cidr_blocks     = ["0.0.0.0/0"]
+    protocol        = "-1"
+    from_port       = 0
+    to_port         = 0
+    prefix_list_ids = []
+  }
+  tags = { Name : "default-${var.environment}-sg" }
+
+}
+
+# setup ec2 instance:
+# fetch the image id programmatically:
+
+data "aws_ami" "latest-aws-linux-image" {
+  most_recent = true
+  owners      = ["amazon"]
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-*-x86_64-gp2"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "server-instance" {
+
+  ami                         = data.aws_ami.latest-aws-linux-image.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.server_subnet.id
+  vpc_security_group_ids      = [aws_default_security_group.default-server-sg.id]
+  availability_zone           = var.az
+  associate_public_ip_address = true
+  key_name                    = "server-key-pair"
+  user_data                   = <<EOF
+                                  #!/bin/bash
+                                  sudo yum update -y && sudo yum install -y docker
+                                  sudo systemctl start docker
+                                  sudo usermod -aG docker ec2-user
+                                  docker run -p 8080:80 nginx
+
+
+                                EOF
+  tags                        = { Name : "${var.environment}-server" }
+
+}
+
+output "aws_instance" {
+  value = aws_instance.server-instance.public_ip
+}
+/*
+output "aws_ami" {
+  value = data.aws_ami.latest-aws-linux-image
+}
+*/
